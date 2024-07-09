bf65beedd9d5c603d070a213f67a3e5765e694f4
Duy Phuong Nguyen <mngduyphuong@gmail.com>
2024-07-09 19:32:06 +1000

    update healthcheck path, update api key config

--- src/main/java/performio/weather_app/controller/WeatherController.java
+++ src/main/java/performio/weather_app/controller/WeatherController.java
@@ -1,7 +1,6 @@
 package performio.weather_app.controller;
 
 // dependencies
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
